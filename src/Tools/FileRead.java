7e6125a53d083c617bb3b2e0d3d28f585a0d7fc7
diego_wxnc9tb <diego1.berrocal@gmail.com>
2021-10-14 11:27:12 +0200

    FileUtils

---
+++ src/Tools/FileRead.java
@@ -0,0 +1,35 @@
+package Tools;
+
+import java.io.*;
+
+public class FileRead {
+    private  BufferedReader  reader;
+    public FileRead(String path){
+        setup(path);
+
+    }
+    public FileRead(){}
+    public boolean setup(String path){
+        File f = new File(path);
+        if(!f.exists()) return false;
+        try {
+            reader= new BufferedReader(new FileReader(f));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }{}
+    public String readNextLine(){
+        if(reader!=null){
+            try {
+                return reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/Tools/FileWrite.java
@@ -0,0 +1,26 @@
+package Tools;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FileWrite {
+    private PrintWriter writer;
+    public FileWrite(String path){
+        setup(path);
+    }
+    public FileWrite(){}
+    public boolean setup(String path){
+        try {
+            writer = new PrintWriter(new FileWriter(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    public PrintWriter writer(){
+        return writer;
+    }
+}
